60705f13dda14a46aa5ab97d4d986f75fa23cc28
mxli <limingxincube@outlook.com>
2021-01-05 10:28:06 +0800

    update ABFormat

--- 02-PAT-Practice-Advanced/AplusBFormat/main.cpp
+++ 02-PAT-Practice-Advanced/AplusBFormat/main.cpp
@@ -2,33 +2,32 @@
 #include <string>
 using namespace std;
 
-int main (){
+int main()
+{
     int a;
     int b;
     cin >> a;
     cin >> b;
-    int sum = a+b;
-    if(sum <0 ){
-        cout << "-";
+    int sum = a + b;
+    if (sum < 0)
+    {
+        cout << '-';
     }
     string s;
-    s = to_string(abs(a+b));
+    s = to_string(abs(a + b));
+    int size = s.length();
     int i;
-    int size =s.length();
-    int head_length = size%3;
-    for(i=0 ;i < head_length;i++){
+    int head_length = size % 3;
+    for (i = 0; i < head_length; i++)
+    {
         cout << s[i];
-        if(size > 3)
-            cout << ",";
     }
-    int k =0;
-    for(;i<size;i++){
-        if(k%3 ==0 && k!=0){
-            cout << ",";
-        }
-        cout << s[i];
-        k++;
+    while (i < size)
+    {
+        if (i != 0)
+            cout << ',';
+        cout << s[i] << s[i + 1] << s[i + 2];
+        i = i + 3;
     }
     cout << endl;
-    return 0;
 }
